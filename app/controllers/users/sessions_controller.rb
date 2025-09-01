3699c5be5e639566a5c96c612acb8d0678dfa38d
enaburekhan <ericenaburekhan2018@gmail.com>
2025-09-01 01:20:13 +0100

    Update user auth

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,2 +1,13 @@
-class Users::SessionsController < ApplicationController
+class Users::SessionsController < Devise::SessionsController
+    respond_to :json
+
+    private
+
+    def respond_with(resource, opt={})
+      render json: { message: "Logged in Successfully", user: resource }, status: :ok
+    end
+
+    def respond_to_on_destroy
+      head :no_content
+    end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,17 +9,19 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  
+
+  # Use JWT instead of cookies/sessions
   config.jwt do |jwt|
-  jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
-  jwt.dispatch_requests = [
-    ['POST', %r{^/login$}]
-  ]
-  jwt.revocation_requests = [
-    ['DELETE', %r{^/logout$}]
-  ]
-  jwt.expiration_time = 1.day.to_i
-end
+    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}],
+      ['POST', %r{^/signup$}]
+    ]
+    jwt.revocation_requests = [
+      ['DELETE', %r{^/logout$}]
+    ]
+    jwt.expiration_time = 1.day.to_i
+  end
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
