68ce926ffbfbd64be078590ebc98c5f985fd2d68
enaburekhan <ericenaburekhan2018@gmail.com>
2025-09-01 17:39:15 +0100

    Test auth on postman

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,9 +5,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def respond_with(resource, _opts = {})
     if resource.persisted?
-      render json: { message: 'Signed up successfully.', user: resource }, status: ok
-    else:
-      render json: { errors: resource.errors.full_messages }, status::unprocessable_entity
+      render json: { message: 'Signed up successfully.', user: resource }, status: :created
+    else
+      render json: { errors: resource.errors.full_messages }, status: :unprocessable_content
     end
+  end
+
+  def sign_up(resource_name, resource)
+    # prevent devise from writing to session.
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,11 +3,21 @@ class Users::SessionsController < Devise::SessionsController
 
     private
 
+    # after login
     def respond_with(resource, opt={})
       render json: { message: "Logged in Successfully", user: resource }, status: :ok
     end
 
+    # after logout
     def respond_to_on_destroy
-      head :no_content
+      if current_user
+        render json: { message: "Logged out successsfully." }, status: :no_content
+      else
+        render json: { error: "User not found." }, status: :unauthorized
+      end
+    end
+
+    def sign_in(resource_name, resource)
+      # do nothing - avoids disabled session error
     end
 end
